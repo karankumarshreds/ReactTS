a6bc25ab94c09f8d8abc8844eb4505dfad382ce5
karankumarshreds <naileditnerd@gmail.com>
2021-02-26 03:31:01 +0530

    add reducer

--- src/redux/reducers/repositoriesReducer.ts
+++ src/redux/reducers/repositoriesReducer.ts
@@ -6,6 +6,12 @@ interface RepositoriesState {
 
 export const repositoriesReducer = (state: RepositoriesState, action: any) => {
   switch (action.type) {
+    case 'SEARCH_REPOS_REQUEST':
+      return { loading: true, error: null, data: [] };
+    case 'SEARCH_REPOS_SUCCESS':
+      return { loading: false, error: null, data: action.payload };
+    case 'SEARCH_REPOS_ERROR':
+      return { loading: false, error: action.payload, data: [] };
     default:
       return state;
   }
