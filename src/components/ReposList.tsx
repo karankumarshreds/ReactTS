39c30442b931b7e75eb9bb27fa88e5e947fd147c
karankumarshreds <naileditnerd@gmail.com>
2021-02-27 00:00:03 +0530

    dispatch action

--- src/components/ReposList.tsx
+++ src/components/ReposList.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import { searchRepos } from '../redux';
+import { useDispatch } from 'react-redux';
 
 const ReposList: React.FC = () => {
   const [term, setTerm] = useState<string>('');
+  const dispatch = useDispatch();
 
-  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Searching');
+    // call the action creator
+    dispatch(searchRepos(term));
   };
+
   return (
     <div>
       <h4>Search Repositories</h4>
