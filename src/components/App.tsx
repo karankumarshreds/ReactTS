804c4039b49bf1075c91756a141473218e6f8ea8
karankumarshreds <naileditnerd@gmail.com>
2021-02-26 22:28:37 +0530

    add provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../redux';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <div>App</div>
+    </Provider>
+  );
 };
 
 export default App;
