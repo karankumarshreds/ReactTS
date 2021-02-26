f82215417f669c1aa7b806e6a596a7aaa49db1fc
karankumarshreds <naileditnerd@gmail.com>
2021-02-26 20:59:25 +0530

    add action interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import ReactDOM from 'react-dom';
-// import Todo from './state/Todo';
-// import Search from './state/Search';
-// import Event from './events/Event';
-import Search from './refs/Search';
 
 const App = () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+  return <div></div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
